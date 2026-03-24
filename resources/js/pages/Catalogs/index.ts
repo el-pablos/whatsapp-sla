27a25f3ed1a04daf89f1004935e8ffb9a167e02c
el-pablos <yeteprem.end23juni@gmail.com>
2026-03-24 11:42:07 +0700

    add: layout components react dengan sidebar collapsible dan header dropdown
    - MainLayout sebagai wrapper utama dengan sidebar & header
    - Sidebar dengan menu: Dashboard, Produk, Pesanan, Chat, Katalog, Pengaturan
    - Header dengan notifikasi dropdown dan user menu
    - Footer dengan copyright
    - Backdrop untuk overlay mobile sidebar
    - Icons components untuk semua menu icons
    - Sidebar context untuk state management (expand/collapse/hover)
    - Mobile-first design dengan responsive breakpoints
    
    el-pablos <yeteprem.end23juni@gmail.com>

---
+++ resources/js/pages/Catalogs/index.ts
@@ -0,0 +1,7 @@
+export { default as CatalogsPage } from './Index'
+export { default as CatalogCard } from './CatalogCard'
+export { default as CatalogForm } from './CatalogForm'
+export { default as ProductSelector } from './ProductSelector'
+export { default as PreviewModal } from './PreviewModal'
+
+export type { Catalog, Product } from './Index'

dc8939adc60805c9713dbbc2b73b3cea1d9fed83
el-pablos <yeteprem.end23juni@gmail.com>
2026-03-24 11:40:32 +0700

    add: update bot init dengan export semua handler functions
    el-pablos <yeteprem.end23juni@gmail.com>

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,5 +1,28 @@
 """
-WhatsApp SLA Bot Package
+WhatsApp Bot Module - Ayam Petelur
 """
+from .handlers import (
+    handle_message,
+    show_main_menu,
+    handle_price,
+    handle_stock,
+    handle_order,
+    handle_catalog,
+    handle_admin,
+    get_session,
+    set_state,
+    clear_session,
+)
 
-__version__ = "1.0.0"
+__all__ = [
+    "handle_message",
+    "show_main_menu",
+    "handle_price",
+    "handle_stock",
+    "handle_order",
+    "handle_catalog",
+    "handle_admin",
+    "get_session",
+    "set_state",
+    "clear_session",
+]
